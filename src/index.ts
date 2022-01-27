69c3af66aea1da6034897eb176c405b929d39dec
danil-kuk <danilkuk5@gmail.com>
2022-01-28 00:05:39 +0500

    Remove key tracking listener

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,3 @@ import { getLastMeme } from './lib/memes';
 
 setInterval(getHeading, 200);
 setInterval(getLastMeme, 1000);
-
-const element = document.getElementById('x') as HTMLDivElement;
-
-document.addEventListener('keydown', event => {
-  element.innerHTML = event.key;
-});
