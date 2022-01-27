daf590adbcf3cfa9bd39a12d8c22c97387d4df65
danil-kuk <danilkuk5@gmail.com>
2022-01-27 23:47:52 +0500

    Add mouseover event listener

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import { getHeading } from './lib/header';
 import { getLastMeme } from './lib/memes';
-import { playSound } from './lib/notifications';
 
 setInterval(getHeading, 200);
 setInterval(getLastMeme, 1000);
-setInterval(playSound, 10000);
+
+const circleElement = document.getElementsByClassName('circle');
+
+document.addEventListener('mousemove', e => {
+  const xPos = e.pageX;
+  const yPos = e.pageY;
+
+  const element = circleElement[0] as HTMLDivElement;
+  element.style.top = `${yPos - 20}px`;
+  element.style.left = `${xPos - 20}px`;
+});
